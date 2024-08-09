f6ea890f7ec3d8072bdf767aae3706e7f123d381
navas <gabrielnavas@protonmail.com>
2024-08-08 23:20:01 -0300

    add focus on course form

--- ui/src/app/courses/course-form/course-form.component.ts
+++ ui/src/app/courses/course-form/course-form.component.ts
@@ -1,33 +1,42 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CoursesService } from '../services/courses.service';
 import { Course } from '../model/course';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss'
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements AfterViewInit {
   form: FormGroup;
   categories = [
-    { id: 'front-end', text: 'Front-end' }, 
+    { id: 'front-end', text: 'Front-end' },
     { id: 'back-end', text: 'Back-end' },
   ];
+
+  @ViewChild('nameInput') nameInput!: ElementRef<MatInput>;
 
   constructor(
     private readonly formBuilder: FormBuilder,
     private readonly router: Router,
     private readonly courseService: CoursesService,
-    private readonly dialog: MatDialog
+    private readonly dialog: MatDialog,
+
   ) {
     this.form = this.formBuilder.group({
       name: [''],
       category: [''],
     })
+
+  }
+  
+  ngAfterViewInit(): void {
+    this.nameInput.nativeElement.focus();
   }
 
   onClickCancel() {
@@ -45,7 +54,7 @@ export class CourseFormComponent {
 
   onClickSave() {
     console.log(this.form.value.name);
-    
+
     const course: Course = {
       id: "0",
       name: this.form.value.name,
@@ -54,6 +63,7 @@ export class CourseFormComponent {
     this.courseService.save(course).subscribe({
       next: () => {
         this.clearForm();
+        this.nameInput.nativeElement.focus();
       }
     })
   }
