4c75bd721807f948ff9d43ac5f0e6933447dfe6f
navas <gabrielnavas@protonmail.com>
2024-08-09 00:41:11 -0300

    add error on select category

--- ui/src/app/courses/course-form/course-form.component.ts
+++ ui/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CoursesService } from '../services/courses.service';
 import { Course } from '../model/course';
@@ -20,7 +20,7 @@ export class CourseFormComponent implements AfterViewInit {
     { id: 'back-end', text: 'Back-end' },
   ];
 
-  @ViewChild('nameInput') nameInput!: ElementRef<MatInput>;
+  @ViewChild('nameInput') nameInput!: ElementRef<HTMLInputElement>;
 
   constructor(
     private readonly formBuilder: FormBuilder,
@@ -28,16 +28,17 @@ export class CourseFormComponent implements AfterViewInit {
     private readonly courseService: CoursesService,
     private readonly dialog: MatDialog,
     private readonly snack: MatSnackBar,
+    private cdr: ChangeDetectorRef
   ) {
     this.form = this.formBuilder.group({
-      name: [''],
-      category: [''],
-    })
-
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+      category: ['', Validators.required]
+    });
   }
-  
+
   ngAfterViewInit(): void {
     this.nameInput.nativeElement.focus();
+    this.cdr.detectChanges(); // Força a detecção de mudanças após a view ter sido inicializada
   }
 
   onClickCancel() {
@@ -54,19 +55,24 @@ export class CourseFormComponent implements AfterViewInit {
   }
 
   onClickSave() {
-    const course: Course = {
-      id: "0",
-      name: this.form.value.name,
-      category: this.form.value.category,
+    if (this.form.invalid) {
+      this.form.markAllAsTouched(); // Marcar todos os campos como "tocados"
+      return;
+    } else {
+      const course: Course = {
+        id: "0",
+        name: this.form.value.name,
+        category: this.form.value.category,
+      }
+      this.courseService.save(course).subscribe({
+        next: () => {
+          this.clearForm();
+          this.nameInput.nativeElement.focus();
+          this.snack.open('Curso criado!', '', { duration: 5000, })
+        },
+        error: (err: Error) => this.showMessage('Atenção!', 'Não foi possível criar um novo curso.')
+      })
     }
-    this.courseService.save(course).subscribe({
-      next: () => {
-        this.clearForm();
-        this.nameInput.nativeElement.focus();
-        this.snack.open('Curso criado!', '', { duration: 5000,  })
-      },
-      error: (err: Error) => this.showMessage('Atenção!', 'Não foi possível criar um novo curso.')
-    })
   }
 
   private clearForm() {
