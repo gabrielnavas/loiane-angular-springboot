370509f84e4d96f137cdfe0686416c41468d73f9
gabrielnavas <gabrielnavas@protonmail.com>
2024-08-13 12:57:34 -0300

    remove unnecessary return type from course controller

--- api/src/main/java/io/github/gabrielnavas/api/course/CourseController.java
+++ api/src/main/java/io/github/gabrielnavas/api/course/CourseController.java
@@ -3,7 +3,6 @@ package io.github.gabrielnavas.api.course;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,44 +16,41 @@ public class CourseController {
     private final CourseService courseService;
 
     @GetMapping
-    public ResponseEntity<List<CourseResponse>> list(
+    public List<CourseResponse> list(
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size
     ) {
-        List<CourseResponse> courses = courseService.list(page, size);
-        return ResponseEntity.ok(courses);
+        return courseService.list(page, size);
     }
 
     @GetMapping("/{course-id}")
-    public ResponseEntity<CourseResponse> getById(
+    public CourseResponse getById(
             @PathVariable("course-id") UUID courseId
     ) {
-        CourseResponse courseResponse = courseService.get(courseId);
-        return ResponseEntity.ok(courseResponse);
+        return courseService.get(courseId);
     }
 
     @PostMapping
-    public ResponseEntity<CourseResponse> save(
+    public CourseResponse save(
             @RequestBody @Valid CourseRequest request
     ) {
-        CourseResponse courseResponse = courseService.save(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(courseResponse);
+        return courseService.save(request);
     }
 
     @PatchMapping("/{course-id}")
-    public ResponseEntity<CourseResponse> partialUpdate(
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void partialUpdate(
             @PathVariable("course-id") UUID courseId,
             @RequestBody @Valid CourseRequest request
     ) {
         courseService.partialUpdate(courseId, request);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @DeleteMapping("/{course-id}")
-    public ResponseEntity<CourseResponse> delete(
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
             @PathVariable("course-id") UUID courseId
     ) {
         courseService.delete(courseId);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
